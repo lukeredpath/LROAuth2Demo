0e64d0ca573d748847163bd241a5ad14b3c051f5
Luke Redpath <luke@lukeredpath.co.uk>
2010-06-01 15:34:49 +0100

    Added example credentials file

---
+++ OAuthCredentials-Example.h
@@ -0,0 +1,12 @@
+/**
+ * Use this to define your credentials.
+ *
+ * You don't want to check this in to version control if you are sharing your code,
+ * you should keep your app secret a secret!
+ *
+ * For the purposes of this Facebook example, your client ID is your API key. 
+ */
+
+#define kOAuthClientID      @"my-client-id"
+#define kOAuthClientSecret  @"my-secret"
+#define kOAuthClientAuthURL @"http://example.com/oauth"

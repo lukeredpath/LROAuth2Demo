319f5c17c47753f13d0dd4d014ea97bb784c9daa
Luke Redpath <luke@lukeredpath.co.uk>
2010-06-01 16:44:21 +0100

    Clean up.

--- Classes/LROAuth2DemoAppDelegate.m
+++ Classes/LROAuth2DemoAppDelegate.m
@@ -17,19 +17,17 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
     
-    // Override point for customization after app launch    
-    [window addSubview:viewController.view];
-    [window makeKeyAndVisible];
+  [window addSubview:viewController.view];
+  [window makeKeyAndVisible];
 
 	return YES;
 }
 
-
-- (void)dealloc {
-    [viewController release];
-    [window release];
-    [super dealloc];
+- (void)dealloc 
+{
+  [viewController release];
+  [window release];
+  [super dealloc];
 }
-
 
 @end
